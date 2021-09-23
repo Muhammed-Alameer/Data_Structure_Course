acb17c5890b314e82bc89fc672e73692fa307b6d
Muhammed Alameer <40340297+Muhammed-Alameer@users.noreply.github.com>
2021-09-23 17:32:15 +0200

    Add files via upload

---
+++ Data Structure/Arrays/Search arrays by java.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+public class Myclass {
+    public static void main(String args[]) {
+                   ArrayList<Integer> myArrayList = new ArrayList<>();
+                   for (int i - 0; i < 10; i++) {
+                      myArrayList.add(i);
+                    }
+                   myArrayList.add(2, 10);
+                   System.out.println()myArrayList);
+
+        System.out.println(myArrayList.indexO(39));
+         System.out.println(myArrayList(9));
+         }
+           }
